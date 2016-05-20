12f7ce6bf6ffa8660e0ee90ed43586baa03774b5
thomasgalliker <thomas@bluewin.ch>
2016-05-20 16:23:12 +0200

    Add IDataSeed and DataSeedBase

---
+++ EntityFramework.Toolkit/IDataSeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace System.Data.Extensions
+{
+    public interface IDataSeed
+    {
+        /// <summary>
+        /// Gets the expression which checks if there are existing seed entries.
+        /// Depending on the result, the seed entry is added or updated.
+        /// </summary>
+        Expression<Func<object, object>> GetAddOrUpdateExpression();
+
+        /// <summary>
+        /// The type of the entity for which this seed is used.
+        /// </summary>
+        Type EntityType { get; }
+
+        /// <summary>
+        /// The seed data as a list of objects.
+        /// </summary>
+        object[] GetAllObjects();
+    }
+}
