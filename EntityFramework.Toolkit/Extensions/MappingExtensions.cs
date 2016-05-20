7726b1b4379c04f1faf38c53fad001668bdac1f8
thomasgalliker <thomas@bluewin.ch>
2016-05-20 16:21:54 +0200

    Add IsUnique extension method

---
+++ EntityFramework.Toolkit/Extensions/MappingExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration.Configuration;
+
+namespace System.Data.Extensions.Extensions
+{
+    public static class MappingExtensions
+    {
+        /// <summary>
+        /// Note: If the unique property is of type string then you have to set the MaxLength.
+        /// </summary>
+        /// <param name="property">The property instance.</param>
+        /// <param name="indexName">The name of the database index. Default is "IX_{some GUID}" if not defined.</param>
+        /// <param name="columnOrder">The column order of the index. Default is 0 if not defined.</param>
+        /// <returns></returns>
+        public static PrimitivePropertyConfiguration IsUnique(
+           this PrimitivePropertyConfiguration property,
+           string indexName = "",
+           int columnOrder = 0)
+        {
+            if (string.IsNullOrEmpty(indexName))
+            {
+                indexName = "IX_" + Guid.NewGuid();
+            }
+
+            var indexAttribute = new IndexAttribute(indexName, columnOrder) { IsUnique = true };
+            var indexAnnotation = new IndexAnnotation(indexAttribute);
+            
+            return property.HasColumnAnnotation(IndexAnnotation.AnnotationName, indexAnnotation);
+        }
+    }
+}
