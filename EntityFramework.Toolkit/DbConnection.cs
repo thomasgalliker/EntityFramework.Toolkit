39a02a580c80061b32f6a023cd1d62c4a991b074
thomasgalliker <thomas@bluewin.ch>
2016-05-20 16:39:31 +0200

    Add DbConnection abstraction

---
+++ EntityFramework.Toolkit/DbConnection.cs
@@ -0,0 +1,17 @@
+﻿namespace System.Data.Extensions
+{
+    public class DbConnection : IDbConnection
+    {
+        public DbConnection(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        public string ConnectionString { get; }
+
+        public override string ToString()
+        {
+            return this.ConnectionString;
+        }
+    }
+}
---
+++ EntityFramework.Toolkit/IDbConnection.cs
@@ -0,0 +1,7 @@
+﻿namespace System.Data.Extensions
+{
+    public interface IDbConnection
+    {
+        string ConnectionString { get; }
+    }
+}
