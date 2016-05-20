79a0b4648c1da325b1def09ebd541cdd420b7d2c
thomasgalliker <thomas@bluewin.ch>
2016-05-20 19:42:59 +0200

    Add data seed unit tests

---
+++  EntityFramework.Toolkit.Tests/DataSeedTests.IntegrationTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Data.Extensions;
+using System.Data.Extensions.Testing;
+using System.Linq;
+
+using FluentAssertions;
+
+using ToolkitSample.DataAccess;
+using ToolkitSample.DataAccess.Context;
+using ToolkitSample.DataAccess.Migrations;
+using ToolkitSample.DataAccess.Model;
+using ToolkitSample.DataAccess.Seed;
+
+using Xunit;
+
+namespace EntityFramework.Toolkit.Tests
+{
+    public class DataSeedTests_IntegrationTests : ContextTestBase<EmployeeContext>
+    {
+        public DataSeedTests_IntegrationTests()
+            : base(dbConnection: new EmployeeContextTestDbConnection(), 
+                   initializeDatabase: false)
+        {
+        }
+
+        [Fact]
+        public void ShouldInitializeContextWithEmptyDataSeed()
+        {
+            // Arrange
+            var emptyDataSeed = new IDataSeed[] { };
+            var databaseInitializer = new EmployeeContextDatabaseInitializer(new EmployeeContextMigrationConfiguration(emptyDataSeed));
+
+            // Act
+            this.InitializeDatabase(databaseInitializer);
+
+            // Assert
+            var allDepartments = this.Context.Set<Department>().ToList();
+            allDepartments.Should().HaveCount(0);
+        }
+
+
+        [Fact]
+        public void ShouldInitializeContextWithDepartmentsSeed()
+        {
+            // Arrange
+            var emptyDataSeed = new IDataSeed[] { new DepartmentDataSeed() };
+            var databaseInitializer = new EmployeeContextDatabaseInitializer(new EmployeeContextMigrationConfiguration(emptyDataSeed));
+
+            // Act
+            this.InitializeDatabase(databaseInitializer);
+
+            // Assert
+            var allDepartments = this.Context.Set<Department>().ToList();
+            allDepartments.Should().HaveCount(2);
+        }
+
+        [Fact(Skip = "Not implemented yet")]
+        public void ShouldInitializeContextTwiceWithDepartmentsSeed()
+        {
+            // Arrange
+            var emptyDataSeed = new IDataSeed[] { new DepartmentDataSeed() };
+            var databaseInitializer = new EmployeeContextDatabaseInitializer(new EmployeeContextMigrationConfiguration(emptyDataSeed));
+
+            // Act
+            this.InitializeDatabase(databaseInitializer);
+            var allDepartments1 = this.Context.Set<Department>().ToList();
+            allDepartments1.Should().HaveCount(2);
+            this.InitializeDatabase(databaseInitializer);
+
+            // Assert
+            var allDepartments2 = this.Context.Set<Department>().ToList();
+            allDepartments2.Should().HaveCount(2);
+        }
+    }
+}
---
+++  EntityFramework.Toolkit.Tests/DataSeedTests.UnitTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Data.Extensions;
+using System.Linq;
+
+using FluentAssertions;
+using ToolkitSample.DataAccess.Model;
+using ToolkitSample.DataAccess.Seed;
+
+using Xunit;
+
+namespace EntityFramework.Toolkit.Tests
+{
+    public class DataSeedTests_UnitTests
+    {
+        [Fact]
+        public void ShouldGetAllObjects()
+        {
+            // Arrange
+            IDataSeed departmentDataSeed = new DepartmentDataSeed();
+
+            // Act
+            var allObject = departmentDataSeed.GetAllObjects();
+
+            // Assert
+            allObject.Should().NotBeNull();
+            allObject.Should().HaveCount(2);
+
+            var allDepartments = allObject.OfType<Department>();
+            allDepartments.Should().HaveCount(2);
+        }
+
+        [Fact]
+        public void ShouldGetAll()
+        {
+            // Arrange
+            var departmentDataSeed = new DepartmentDataSeed();
+
+            // Act
+            var allDepartments = departmentDataSeed.GetAll();
+
+            // Assert
+            allDepartments.Should().NotBeNull();
+            allDepartments.Should().HaveCount(2);
+        }
+    }
+}
