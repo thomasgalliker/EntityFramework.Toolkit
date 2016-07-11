05914c45a8b3f7f1b9c3de10e18df8161f26b661
thomasgalliker <thomas@bluewin.ch>
2016-07-11 23:09:38 +0200

    Add unit test for GenericRepository.Update method

---  EntityFramework.Toolkit.Tests/Repository/EmployeeRepositoryTests.cs
+++  EntityFramework.Toolkit.Tests/Repository/EmployeeRepositoryTests.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 using EntityFramework.Toolkit.Core;
+using EntityFramework.Toolkit.Exceptions;
 using EntityFramework.Toolkit.Extensions;
 using EntityFramework.Toolkit.Testing;
 using EntityFramework.Toolkit.Tests.Stubs;
@@ -14,6 +17,7 @@ using ToolkitSample.DataAccess.Repository;
 using ToolkitSample.Model;
 
 using Xunit;
+using Xunit.Abstractions;
 
 namespace EntityFramework.Toolkit.Tests.Repository
 {
@@ -22,9 +26,12 @@ namespace EntityFramework.Toolkit.Tests.Repository
     /// </summary>
     public class EmployeeRepositoryTests : ContextTestBase<EmployeeContext>
     {
-        public EmployeeRepositoryTests()
+        private ITestOutputHelper testOutputHelper;
+
+        public EmployeeRepositoryTests(ITestOutputHelper testOutputHelper)
             : base(dbConnection: new EmployeeContextTestDbConnection())
         {
+            this.testOutputHelper = testOutputHelper;
         }
 
         [Fact]
@@ -54,9 +61,9 @@ namespace EntityFramework.Toolkit.Tests.Repository
             // Assert
             AssertChangeSet(committedChangeSet, numberOfAdded: 1, numberOfModified: 0, numberOfDeleted: 0);
 
-                        var getEmployee = employeeRepository.Get()
-                .Include(d => d.Department)
-                .Single(e => e.FirstName == employee.FirstName);
+            var getEmployee = employeeRepository.Get()
+    .Include(d => d.Department)
+    .Single(e => e.FirstName == employee.FirstName);
 
             getEmployee.ShouldBeEquivalentTo(CreateEntity.Employee1, options => options.IncludingAllDeclaredProperties());
         }
@@ -152,7 +159,7 @@ namespace EntityFramework.Toolkit.Tests.Repository
             employeeRepository.Save();
 
             // Act
-            employeeRepository.RemoveRange(new [] { CreateEntity.Employee1, CreateEntity.Employee3 });
+            employeeRepository.RemoveRange(new[] { CreateEntity.Employee1, CreateEntity.Employee3 });
             var committedChangeSet = employeeRepository.Save();
 
             // Assert
@@ -161,6 +168,92 @@ namespace EntityFramework.Toolkit.Tests.Repository
             var allEmployees = employeeRepository.GetAll().ToList();
             allEmployees.Should().HaveCount(1);
             allEmployees.ElementAt(0).ShouldBeEquivalentTo(CreateEntity.Employee2, options => options.IncludingAllDeclaredProperties());
+        }
+
+        [Fact]
+        public void ShouldUpdateExistingEmployee()
+        {
+            // Arrange
+            IEmployeeRepository employeeRepository = new EmployeeRepository(this.Context);
+            var employee1 = employeeRepository.Add(CreateEntity.Employee1);
+            var employee2 = employeeRepository.Add(CreateEntity.Employee2);
+            employeeRepository.Save();
+
+            var employee1Update = CreateEntity.Employee1;
+            employee1Update.FirstName = "Updated " + employee1Update.FirstName;
+
+            // Act
+            employeeRepository.Update(employee1Update);
+            var committedChangeSet = employeeRepository.Save();
+
+            // Assert
+            AssertChangeSet(committedChangeSet, numberOfAdded: 0, numberOfModified: 1, numberOfDeleted: 0);
+
+            var allEmployees = employeeRepository.GetAll().ToList();
+            allEmployees.Should().HaveCount(2);
+            allEmployees.Single(e => e.Id == employee1.Id).ShouldBeEquivalentTo(employee1Update, options => options.IncludingAllDeclaredProperties());
+            allEmployees.Single(e => e.Id == employee2.Id).ShouldBeEquivalentTo(employee2, options => options.IncludingAllDeclaredProperties());
+        }
+
+        [Fact]
+        public void ShouldUpdateExistingEmployees()
+        {
+            // Arrange
+            IEmployeeRepository employeeRepository = new EmployeeRepository(this.Context);
+            var employees = new List<Employee> { Testdata.Employees.CreateEmployee1(), Testdata.Employees.CreateEmployee2(), Testdata.Employees.CreateEmployee3() };
+            employees = employeeRepository.AddRange(employees).ToList();
+            employeeRepository.Save();
+
+            employeeRepository = new EmployeeRepository(this.CreateContext());
+            int n = 5000;
+            var timespanOffset = new TimeSpan(0, 0, 0, 1);
+
+            // Act
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < n; i++)
+            {
+                foreach (var updateEmployee in employees)
+                {
+                    updateEmployee.Birthdate += timespanOffset;
+                    employeeRepository.Update(updateEmployee);
+                }
+            }
+            var committedChangeSet = employeeRepository.Save();
+            stopwatch.Stop();
+            this.testOutputHelper.WriteLine($"Elapsed={stopwatch.ElapsedMilliseconds}ms");
+
+            // Assert
+            AssertChangeSet(committedChangeSet, numberOfAdded: 0, numberOfModified: 3, numberOfDeleted: 0);
+
+            employeeRepository = new EmployeeRepository(this.CreateContext());
+            var allEmployees = employeeRepository.GetAll().ToList();
+            allEmployees.Should().HaveCount(3);
+            allEmployees.ElementAt(0).Birthdate.Should().Be(new DateTime(1986, 07, 11, 01, 23, 20));
+            allEmployees.ElementAt(1).Birthdate.Should().Be(new DateTime(1990, 01, 01, 01, 23, 20));
+            allEmployees.ElementAt(2).Birthdate.Should().Be(new DateTime(2000, 12, 31, 01, 23, 20));
+
+            stopwatch.ElapsedMilliseconds.Should().BeLessThan(1000);
+        }
+
+        [Fact]
+        public void ShouldThrowUpdateConcurrencyExceptionWhenTryingToUpdateNonExistingEntity()
+        {
+            // Arrange
+            IEmployeeRepository employeeRepository = new EmployeeRepository(this.Context);
+            var employee1 = Testdata.Employees.CreateEmployee1();
+            employee1.Id = 99;
+
+            // Act
+            employeeRepository.Update(employee1);
+            Action action = () => employeeRepository.Save();
+
+            // Assert
+            action.ShouldThrow<UpdateConcurrencyException>();
+
+            employeeRepository = new EmployeeRepository(this.CreateContext());
+            var allEmployees = employeeRepository.GetAll().ToList();
+            allEmployees.Should().HaveCount(0);
         }
 
         private static void AssertChangeSet(ChangeSet changeSet, int numberOfAdded, int numberOfModified, int numberOfDeleted)
---  EntityFramework.Toolkit.Tests/Stubs/Create.cs
+++  EntityFramework.Toolkit.Tests/Stubs/Create.cs
@@ -5,6 +5,26 @@ using ToolkitSample.Model;
 
 namespace EntityFramework.Toolkit.Tests.Stubs
 {
+    public static class Testdata
+    {
+        public static class Employees
+        {
+            public static Employee CreateEmployee1()
+            {
+                return new Employee { FirstName = "Thomas", LastName = "Galliker", Birthdate = new DateTime(1986, 07, 11) };
+            }
+
+            public static Employee CreateEmployee2()
+            {
+                return new Employee { FirstName = "Fritz", LastName = "Müller", Birthdate = new DateTime(1990, 01, 01) };
+            }
+
+            public static Employee CreateEmployee3()
+            {
+                return new Employee { FirstName = "Lorem", LastName = "Ipsum", Birthdate = new DateTime(2000, 12, 31) };
+            }
+        }
+    }
     public static class CreateEntity
     {
         public static readonly Employee Employee1 = new Employee { FirstName = "Thomas", LastName = "Galliker", Birthdate = new DateTime(1986, 07, 11) };
