6eeae06124b075116264a4996baded10c914091f
thomasgalliker <thomas@bluewin.ch>
2016-07-14 10:01:22 +0200

    Add tests for new data seeds

---  EntityFramework.Toolkit.Tests/DataSeedTests.IntegrationTests.cs
+++  EntityFramework.Toolkit.Tests/DataSeedTests.IntegrationTests.cs
@@ -26,8 +26,8 @@ namespace EntityFramework.Toolkit.Tests
         public void ShouldInitializeContextWithEmptyDataSeed()
         {
             // Arrange
-            var emptyDataSeed = new IDataSeed[] { };
-            var databaseInitializer = new EmployeeContextDatabaseInitializer(new EmployeeContextMigrationConfiguration(emptyDataSeed));
+            var dataSeed = new IDataSeed[] { };
+            var databaseInitializer = new EmployeeContextDatabaseInitializer(new EmployeeContextMigrationConfiguration(dataSeed));
 
             // Act
             this.InitializeDatabase(databaseInitializer);
@@ -42,8 +42,8 @@ namespace EntityFramework.Toolkit.Tests
         public void ShouldInitializeContextWithDepartmentsSeed()
         {
             // Arrange
-            var emptyDataSeed = new IDataSeed[] { new DepartmentDataSeed() };
-            var databaseInitializer = new EmployeeContextDatabaseInitializer(new EmployeeContextMigrationConfiguration(emptyDataSeed));
+            var dataSeed = new IDataSeed[] { new DepartmentDataSeed() };
+            var databaseInitializer = new EmployeeContextDatabaseInitializer(new EmployeeContextMigrationConfiguration(dataSeed));
 
             // Act
             this.InitializeDatabase(databaseInitializer);
@@ -54,11 +54,26 @@ namespace EntityFramework.Toolkit.Tests
         }
 
         [Fact]
+        public void ShouldInitializeContextWithApplicationSettingSeed()
+        {
+            // Arrange
+            var dataSeed = new IDataSeed[] { new ApplicationSettingDataSeed(),  };
+            var databaseInitializer = new EmployeeContextDatabaseInitializer(new EmployeeContextMigrationConfiguration(dataSeed));
+
+            // Act
+            this.InitializeDatabase(databaseInitializer);
+
+            // Assert
+            var applicationSetting = this.Context.Set<ApplicationSetting>().ToList();
+            applicationSetting.Should().HaveCount(1);
+        }
+
+        [Fact]
         public void ShouldInitializeContextTwiceWithDepartmentsSeed()
         {
             // Arrange
-            var emptyDataSeed = new IDataSeed[] { new DepartmentDataSeed() };
-            var databaseInitializer = new EmployeeContextDatabaseInitializer(new EmployeeContextMigrationConfiguration(emptyDataSeed));
+            var dataSeed = new IDataSeed[] { new DepartmentDataSeed() };
+            var databaseInitializer = new EmployeeContextDatabaseInitializer(new EmployeeContextMigrationConfiguration(dataSeed));
 
             // Act
             this.InitializeDatabase(databaseInitializer);
@@ -71,6 +86,26 @@ namespace EntityFramework.Toolkit.Tests
             // Assert
             var allDepartments2 = this.Context.Set<Department>().ToList();
             allDepartments2.Should().HaveCount(2);
+        }
+
+        [Fact]
+        public void ShouldInitializeContextTwiceWithApplicationSettingSeed()
+        {
+            // Arrange
+            var dataSeed = new IDataSeed[] { new ApplicationSettingDataSeed() };
+            var databaseInitializer = new EmployeeContextDatabaseInitializer(new EmployeeContextMigrationConfiguration(dataSeed));
+
+            // Act
+            this.InitializeDatabase(databaseInitializer);
+
+            var applicationSettings1 = this.Context.Set<ApplicationSetting>().ToList();
+            applicationSettings1.Should().HaveCount(1);
+
+            this.InitializeDatabase(databaseInitializer);
+
+            // Assert
+            var applicationSettings2 = this.Context.Set<ApplicationSetting>().ToList();
+            applicationSettings2.Should().HaveCount(1);
         }
     }
 }
