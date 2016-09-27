927a92d2f1746bc40c6720210a269a4dba3d4a72
thomasgalliker <thomas@bluewin.ch>
2016-09-27 07:55:43 +0200

    Remove static test data

---  EntityFramework.Toolkit.Tests/Stubs/Create.cs
+++  EntityFramework.Toolkit.Tests/Stubs/Create.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 using ToolkitSample.Model;
 
@@ -35,14 +34,5 @@ namespace EntityFramework.Toolkit.Tests.Stubs
                 };
             }
         }
-    }
-    public static class CreateEntity
-    {
-        public static readonly Employee Employee1 = new Employee { FirstName = "Thomas", LastName = "Galliker", Birthdate = new DateTime(1986, 07, 11) };
-        public static readonly Employee Employee2 = new Employee { FirstName = "Fritz", LastName = "Müller", Birthdate = new DateTime(1990, 01, 01) };
-        public static readonly Employee Employee3 = new Employee { FirstName = "Lorem", LastName = "Ipsum", Birthdate = new DateTime(2000, 12, 31) };
-
-        public static readonly Department Department1 = new Department { Name = "Human Resources", Leader = Employee1, Employees = new List<Employee> { Employee2, Employee3 } };
-        public static readonly Department Department2 = new Department { Name = "Development", Leader = Employee2, Employees = new List<Employee> { Employee1, Employee3 } };
     }
 }
