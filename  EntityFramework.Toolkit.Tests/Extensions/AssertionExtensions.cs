e9318ea445b14bc94886eb2120e96de5e64a9a4f
thomasgalliker <thomas@bluewin.ch>
2016-09-27 16:02:32 +0200

    Add AssertionExtensions for better comparison of entity vs. poco in tests

---
+++  EntityFramework.Toolkit.Tests/Extensions/AssertionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using ToolkitSample.Model;
+
+namespace EntityFramework.Toolkit.Tests.Extensions
+{
+    internal static class AssertionExtensions
+    {
+        internal static void ShouldBeEquivalentTo(this Employee subject, Employee expectedEmployee)
+        {
+            subject.ShouldBeEquivalentTo(expectedEmployee, options => options.IncludingAllDeclaredProperties()
+                                                                             .IgnoringCyclicReferences()
+                                                                             .Excluding(e => e.Id)
+                                                                             .Excluding(e => e.Department)
+                                                                             .Excluding(e => e.DepartmentId)
+                                                                             .Excluding(e => e.RowVersion));
+        }
+    }
+}
