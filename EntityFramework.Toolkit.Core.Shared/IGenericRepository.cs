e5df7a4e128be5f3a78467a6b6ec14e3e732b37c
thomasgalliker <thomas@bluewin.ch>
2016-11-11 00:11:58 +0100

    Move readonly methods to IReadOnlyRepository

--- EntityFramework.Toolkit.Core.Shared/IGenericRepository.cs
+++ EntityFramework.Toolkit.Core.Shared/IGenericRepository.cs
@@ -8,43 +8,9 @@ namespace EntityFramework.Toolkit.Core
     /// <summary>
     ///     Abstraction of a generic repository.
     /// </summary>
-    public interface IGenericRepository<T> : IRepository
+    public interface IGenericRepository<T> : IWritableRepository, IReadOnlyRepository<T>
     {
         IContext Context { get; }
-
-        /// <summary>
-        ///     Returns a collection of all entities in the context, or that can be queried from the
-        ///     database, of given type <typeparamref name="T" />.
-        /// </summary>
-        IQueryableIncluding<T> Get();
-
-        [Obsolete("Use GenericRepository.Get().Include(...) instead")]
-        IQueryable<T> Get(params Expression<Func<T, object>>[] includes);
-
-        /// <summary>
-        ///     Returns a collection of all entities in the context, or that can be queried from the
-        ///     database, of given type <typeparamref name="T" />.
-        /// </summary>
-        IEnumerable<T> GetAll();
-
-        /// <summary>
-        ///     Finds an entity with the given primary key values.
-        ///     If an entity with the given primary key values exists in the context, then it is
-        ///     returned immediately without making a request to the store.  Otherwise, a request
-        ///     is made to the store for an entity with the given primary key values and this entity,
-        ///     if found, is attached to the context and returned.  If no entity is found in the
-        ///     context or the store, then null is returned.
-        /// </summary>
-        /// <param name="ids">The values of the primary key for the entity to be found. </param>
-        /// <returns> The entity found, or null. </returns>
-        T FindById(params object[] ids);
-
-        /// <summary>
-        ///     Finds entities with the given search predicate.
-        /// </summary>
-        /// <param name="predicate">The search predicate.</param>
-        /// <returns>A collection of entities matching the search predicate.</returns>
-        IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate);
 
         /// <summary>
         ///     Adds the given entity to the context underlying the set in the Added state such that it will
---
+++ Samples/ToolkitSample.DataAccess/Repository/EmployeeReadOnlyRepository.cs
@@ -0,0 +1,21 @@
+﻿
+using EntityFramework.Toolkit;
+
+using ToolkitSample.DataAccess.Context;
+using ToolkitSample.DataAccess.Contracts.Repository;
+using ToolkitSample.Model;
+
+namespace ToolkitSample.DataAccess.Repository
+{
+    /// <summary>
+    /// The EmployeeReadOnlyRepository should only be visibile withing the DAL layer.
+    /// Consumers have to consume the IEmployeeReadOnlyRepository interface.
+    /// </summary>
+    public class EmployeeReadOnlyRepository : GenericRepository<Employee>, IEmployeeReadOnlyRepository
+    {
+        public EmployeeReadOnlyRepository(IEmployeeContext context)
+            : base(context)
+        {
+        }
+    }
+}
