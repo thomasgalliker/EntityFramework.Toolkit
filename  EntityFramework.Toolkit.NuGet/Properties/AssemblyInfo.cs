5987efc562e7cb661aa9b319668698557c61e830
thomasgalliker <thomas@bluewin.ch>
2017-01-19 19:23:57 +0100

    Update assembly info

---  EntityFramework.Toolkit.NuGet/Properties/AssemblyInfo.cs
+++  EntityFramework.Toolkit.NuGet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Thomas Galliker")]
 [assembly: AssemblyProduct("EntityFramework.Toolkit.NuGet")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
