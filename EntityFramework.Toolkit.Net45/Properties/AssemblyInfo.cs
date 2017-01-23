d719c40f8b41c1fd9314c12f539f16986f9fde03
thomasgalliker <thomas@bluewin.ch>
2017-01-23 20:21:52 +0100

    Update assembly info

--- EntityFramework.Toolkit.Net45/Properties/AssemblyInfo.cs
+++ EntityFramework.Toolkit.Net45/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.4")]
 [assembly: AssemblyVersion("1.0.4")]
-[assembly: AssemblyFileVersion("1.0.4-pre92")]
+[assembly: AssemblyFileVersion("1.0.4-pre93")]
 [assembly: InternalsVisibleTo("EntityFramework.Toolkit.Tests")]
