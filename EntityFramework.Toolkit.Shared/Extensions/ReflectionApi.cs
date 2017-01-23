660e5420b753094f7177a9b0ebda10c809f7be3b
thomasgalliker <thomas@bluewin.ch>
2017-01-23 23:49:39 +0100

    Extend ReflectionApi for .Net 4.0 with PropertyInfo.SetValue

--- EntityFramework.Toolkit.Shared/Extensions/ReflectionApi.cs
+++ EntityFramework.Toolkit.Shared/Extensions/ReflectionApi.cs
@@ -1,4 +1,6 @@
-﻿#if NET40
+﻿using System.Reflection;
+
+#if NET40
 
 namespace System
 {
@@ -7,6 +9,14 @@ namespace System
         internal static bool IsGenericType(this Type t)
         {
             return t.IsGenericType;
+        }
+    }
+
+    internal static class PropertyInfoExtensions
+    {
+        internal static void SetValue(this PropertyInfo propertyInfo, object obj, object value)
+        {
+            propertyInfo.SetValue(obj, value, null);
         }
     }
 }
