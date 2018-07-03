3d9b4ae33e7962c71a48f8a36de79554ad0810da
Thomas Galliker <thomas@bluewin.ch>
2018-07-03 23:45:15 +0200

    Release 1.0.6-pre8

--- EntityFramework.Toolkit/Properties/AssemblyInfo.cs
+++ EntityFramework.Toolkit/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.6")]
 [assembly: AssemblyVersion("1.0.6")]
-[assembly: AssemblyFileVersion("1.0.6-pre7")]
+[assembly: AssemblyFileVersion("1.0.6-pre8")]
