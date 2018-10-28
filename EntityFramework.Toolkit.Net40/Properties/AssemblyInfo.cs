1b45f172cd28310417d460e970f6e80914a70a2e
Thomas Galliker <thomas@bluewin.ch>
2018-10-28 13:43:44 +0100

    Exclude nuget proj from build

--- EntityFramework.Toolkit.Net40/Properties/AssemblyInfo.cs
+++ EntityFramework.Toolkit.Net40/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.7")]
 [assembly: AssemblyVersion("1.0.7")]
-[assembly: AssemblyFileVersion("1.0.7-pre5")]
+[assembly: AssemblyFileVersion("1.0.7-pre6")]
 [assembly: InternalsVisibleTo("EntityFramework.Toolkit.Tests")]
